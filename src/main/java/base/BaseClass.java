0bdf573bb471af807658d609c105d7febed44948
fahim59 <mrahaman59@yahoo.com>
2023-09-09 23:33:31 +0600

    code update

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -5,10 +5,12 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,12 +20,15 @@ import utils.ConfigLoader;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BaseClass {
     protected static AndroidDriver<AndroidElement> driver;
+    private static AppiumDriverLocalService server;
 
     private static final Logger logger = LogManager.getLogger(BaseClass.class);
 
@@ -67,6 +72,43 @@ public class BaseClass {
             catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @BeforeSuite
+    public void beforeSuite() throws Exception {
+        ThreadContext.put("ROUTINGKEY", "ServerLogs");
+        server = AppiumDriverLocalService.buildDefaultService();
+
+        if(!checkIfAppiumServerIsRunnning(4723)) {
+            server.start();
+            server.clearOutPutStreams();
+
+            logger.info("Appium server started");
+        }
+        else {
+            logger.info("Appium server already running");
+        }
+    }
+
+    public boolean checkIfAppiumServerIsRunnning(int port) {
+        boolean isAppiumServerRunning = false;
+        ServerSocket socket;
+        try {
+            socket = new ServerSocket(port);
+            socket.close();
+        }
+        catch (IOException e) {
+            isAppiumServerRunning = true;
+        }
+        return isAppiumServerRunning;
+    }
+
+    @AfterSuite (alwaysRun = true)
+    public void afterSuite() {
+        if(server.isRunning()){
+            server.stop();
+            logger.info("Appium server stopped");
         }
     }
 
